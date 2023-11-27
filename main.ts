d1c81c757231675507f713a4f0264ee0f4b1233d
Atri-Sarker <152197131+Atri-Sarker@users.noreply.github.com>
2023-11-27 11:04:16 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+let projectile: Sprite = null
+scene.setBackgroundColor(9)
+let mySprite = sprites.create(img`
+    . . f f f f f f f f f f . . . . 
+    . f f 5 5 5 5 5 5 5 5 f . . . . 
+    . f 5 5 5 e 4 e 5 5 5 f . . . . 
+    f f f 5 e 4 e 4 e 5 5 f f f . . 
+    f 5 5 5 4 e 4 e 4 e 5 5 5 f . . 
+    f 5 5 5 e 4 e 4 e 4 5 5 5 f . . 
+    f f f f 5 e 4 e 4 5 f f f f . . 
+    . f 5 5 5 5 e 4 5 5 5 f . . . . 
+    . f 5 5 5 f 7 f 5 5 5 f . . . . 
+    . f f f f f 7 f f f 5 f . . . . 
+    . . . f f f 7 f f f f f . . . . 
+    . . . f 7 7 7 7 7 f . . . . . . 
+    . f f f 7 f 7 f f f f f . . . . 
+    . f 7 f f f 7 f f f 7 f . . . . 
+    . f 7 7 7 7 7 7 7 7 7 f . . . . 
+    . f f f f f f f f f f f . . . . 
+    `, SpriteKind.Player)
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f . f . . . . . . . 
+        . . . . . d d f d d d d . . . . 
+        . . . d d d f 5 f 5 5 d d . . . 
+        . d d d f 5 f 5 f 5 f 5 d . . . 
+        . d f f f 5 f 5 f 5 5 5 d . . . 
+        . d d d f 5 f 5 f 5 5 5 d . . . 
+        . . . d d d f 5 f 5 5 d d . . . 
+        . . . . . d d d d d d d . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, randint(-25, 25), randint(-25, 25))
+    projectile.lifespan = 3000
+    if (mySprite.vx < 0) {
+        projectile.image.flipX()
+    }
+})
